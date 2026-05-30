8777f65392c836920bcfb06066075d014a881c8e
Jon <due@wxwhatever.com>
2026-05-30 13:50:32 +1000

    Register item renderer.

---
+++ neoforge/src/main/java/noobanidus/mods/twilightlootr/client/item/TFLootrBossChestItemRenderer.java
@@ -0,0 +1,56 @@
+package noobanidus.mods.twilightlootr.client.item;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import noobanidus.mods.lootr.common.api.registry.LootrRegistry;
+import noobanidus.mods.lootr.common.block.entity.LootrChestBlockEntity;
+import noobanidus.mods.lootr.common.client.item.LootrChestItemRenderer;
+import noobanidus.mods.lootr.common.entity.LootrChestMinecartEntity;
+import noobanidus.mods.twilightlootr.block.entity.TFLootrBossChestBlockEntity;
+import noobanidus.mods.twilightlootr.init.ModBlocks;
+
+public class TFLootrBossChestItemRenderer extends BlockEntityWithoutLevelRenderer {
+  private static TFLootrBossChestItemRenderer INSTANCE = null;
+
+  private final BlockEntityRenderDispatcher blockEntityRenderDispatcher;
+  private final TFLootrBossChestBlockEntity blockEntity;
+
+  public TFLootrBossChestItemRenderer(BlockEntityRenderDispatcher pBlockEntityRenderDispatcher, EntityModelSet pEntityModelSet) {
+    super(pBlockEntityRenderDispatcher, pEntityModelSet);
+    this.blockEntityRenderDispatcher = pBlockEntityRenderDispatcher;
+    this.blockEntity = new TFLootrBossChestBlockEntity(BlockPos.ZERO, ModBlocks.BOSS_CHEST.value().defaultBlockState());
+  }
+
+  public TFLootrBossChestItemRenderer() {
+    this(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels());
+  }
+
+  public static TFLootrBossChestItemRenderer getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new TFLootrBossChestItemRenderer();
+    }
+
+    return INSTANCE;
+  }
+
+  @Override
+  public void renderByItem(ItemStack p_108830_, ItemDisplayContext p_270899_, PoseStack p_108832_, MultiBufferSource p_108833_, int p_108834_, int p_108835_) {
+    this.blockEntityRenderDispatcher.renderItem(blockEntity, p_108832_, p_108833_, p_108834_, p_108835_);
+  }
+
+  public void renderByMinecart(LootrChestMinecartEntity entity, PoseStack matrixStack, MultiBufferSource buffer, int combinedLight) {
+    // Don't change this to `hasClientOpened`
+    boolean open = blockEntity.isClientOpened();
+    blockEntity.setClientOpened(entity.isClientOpened());
+    this.blockEntityRenderDispatcher.renderItem(blockEntity, matrixStack, buffer, combinedLight, OverlayTexture.NO_OVERLAY);
+    blockEntity.setClientOpened(open);
+  }
+}
--- neoforge/src/main/java/noobanidus/mods/twilightlootr/setup/ClientSetup.java
+++ neoforge/src/main/java/noobanidus/mods/twilightlootr/setup/ClientSetup.java
@@ -1,16 +1,21 @@
 package noobanidus.mods.twilightlootr.setup;
 
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.ModelEvent;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 import noobanidus.mods.lootr.common.api.registry.LootrRegistry;
 import noobanidus.mods.lootr.common.block.entity.LootrChestBlockEntity;
 import noobanidus.mods.twilightlootr.TwilightLootr;
 import noobanidus.mods.twilightlootr.client.block.TFLootrBossChestBlockRenderer;
+import noobanidus.mods.twilightlootr.client.item.TFLootrBossChestItemRenderer;
 import noobanidus.mods.twilightlootr.init.ModBlockEntities;
+import noobanidus.mods.twilightlootr.init.ModItems;
 
 @EventBusSubscriber(modid = TwilightLootr.MODID, value = Dist.CLIENT)
 public class ClientSetup {
@@ -21,5 +26,15 @@ public class ClientSetup {
   @SubscribeEvent
   public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
     event.registerBlockEntityRenderer(ModBlockEntities.BOSS_CHEST.get(), TFLootrBossChestBlockRenderer::new);
+  }
+
+  @SubscribeEvent
+  public static void registerExtensions (RegisterClientExtensionsEvent event) {
+    event.registerItem(new IClientItemExtensions() {
+      @Override
+      public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+        return TFLootrBossChestItemRenderer.getInstance();
+      }
+    }, ModItems.BOSS_CHEST.get());
   }
 }
